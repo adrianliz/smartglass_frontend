1712812d151d49768a09990ed97696a0e1799415
Adrián Lizaga <759090@unizar.es>
2021-09-26 18:03:33 +0200

    update: dependencies and footer component

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { name, version } from '../../../../../package.json';
+import pkgInfo from '../../../../../package.json';
 
 @Component({
 	selector: 'app-footer',
 	templateUrl: './footer.component.html',
 })
 export class FooterComponent {
-	version = version;
-	name = name;
+	version = pkgInfo.version;
+	name = pkgInfo.name;
 
 	constructor() {}
 }
